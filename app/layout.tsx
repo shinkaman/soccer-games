705bb1c4324ac057763fcbcff8855bbb6e723600
Yohei Toda <yoheitoda@YoheinoMacBook-Pro.local>
2026-01-26 22:26:10 +0900

    Set favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'サッカー試合日程一覧',
   description: '欧州5大リーグ、Jリーグ、日本代表などの試合日程を一覧表示',
   icons: {
-    icon: '/favicon.png',
+    icon: '/favicon.ico',
   },
 }
 
